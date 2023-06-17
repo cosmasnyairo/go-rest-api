66fabd6caceb9e8135f44c6af8b2cbf7ecb66c80
Cosmas Nyairo <nyairocosmas@gmail.com>
2023-06-17 17:52:03 +0300

    add database connection logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/cosmasnyairo/go-rest-api/internal/db"
+)
+
+var (
+	SuccessfullyConnectedToDB = "successfully to connect to database"
+)
 
 // Responsible for instantiation and startup of our go application
 func Run() error {
 	fmt.Println("Starting our application")
+	db, err := db.NewDatabase()
+	if err != nil {
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println(SuccessfullyConnectedToDB)
 	return nil
 }
 func main() {
--- go.mod
+++ go.mod
@@ -2,3 +2,8 @@ module github.com/cosmasnyairo/go-rest-api
 
 // replace github.com/cosmasnyairo/go-rest-api v0.0.1 => ./github
 go 1.20
+
+require (
+	github.com/jmoiron/sqlx v1.3.5 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
