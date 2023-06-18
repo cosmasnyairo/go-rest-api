9c7eaf93005d1b65993c89a7b2fd32f4bdfbbebb
Cosmas Nyairo <nyairocosmas@gmail.com>
2023-06-18 16:50:56 +0300

    add gracefull shutdown

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,8 +1,13 @@
 package http
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -40,9 +45,20 @@ func (h *Handler) MapRoutes() {
 }
 
 func (h *Handler) Serve() error {
-	if err := h.Server.ListenAndServe(); err != nil {
-		return err
-	}
+	go func() {
+		if err := h.Server.ListenAndServe(); err != nil {
+			log.Println(err.Error())
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	h.Server.Shutdown(ctx)
+	log.Println("Gracefully Shutdown")
 	return nil
 
 }
